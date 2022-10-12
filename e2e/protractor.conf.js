89ba0e37a3875cd7a46cf68435259cdd5b5d064a
Shivam Mishra <shivamkumar6370@gmail.com>
2022-10-12 16:56:28 +0530

    Update protractor.conf.js

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -10,6 +10,15 @@ exports.config = {
   specs: ['./src/**/*.e2e-spec.ts'],
   capabilities: {
     browserName: 'chrome',
+      },
+  directConnect: true,
+  baseUrl: 'http://localhost:4200/',
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    print: function() {},
+  },
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
